83e2ed6b4bfd73b385e0f47c692db17fa86ac67b
andry81 <andry@inbox.ru>
2024-07-04 10:02:09 +0300

    * added `git_cleanup_original_refs.sh` and `git_cleanup_replace_refs.sh` wrapper script

---
+++ cleanup-original-refs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+"$PROJECTS_ROOT"'/andry81/gitcmd/gitcmd/git_cleanup_original_refs.sh' "$@"
---
+++ cleanup-replace-refs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+"$PROJECTS_ROOT"'/andry81/gitcmd/gitcmd/git_cleanup_replace_refs.sh' "$@"
